563c5d7d4c01aecd35c1718f2e3b5aa059aa51ca
unknown <Shi Xiao>
2017-02-24 17:00:45 +0800

    Complete Lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -13,6 +13,15 @@ public class LeapYear {
             System.out.printf("%d is not a leap year.\n", year);
         }
     }
+    public static boolean isLeapYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+        } else if (year % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
