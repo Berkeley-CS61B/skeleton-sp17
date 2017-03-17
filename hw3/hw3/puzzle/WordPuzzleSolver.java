b6be4c1dde862bb584affc40a71808787ebc8875
Josh Hug <hug@cs.berkeley.edu>
2017-03-16 23:26:17 -0700

    added hw3 files

---
+++ hw3/hw3/puzzle/WordPuzzleSolver.java
@@ -0,0 +1,22 @@
+package hw3.puzzle;
+import edu.princeton.cs.algs4.StdOut;
+
+public class WordPuzzleSolver {
+    /***********************************************************************
+     * Test routine for your Solver class. Uncomment and run to test
+     * your basic functionality. Make sure to set your current working directory
+     * to be the one containing words10000.txt.
+     **********************************************************************/
+    /*public static void main(String[] args) {
+        String start = "cube";
+        String goal = "tubes";
+
+        Word startState = new Word(start, goal);
+        Solver solver = new Solver(startState);
+
+        StdOut.println("Minimum number of moves = " + solver.moves());
+        for (WorldState ws : solver.solution()) {
+            StdOut.println(ws);
+        }
+    }*/
+}
