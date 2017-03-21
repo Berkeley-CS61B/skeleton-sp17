e9ec94450da2546a0fe9a864d8a88015895126ea
Josh hug <hug@cs.berkeley.edu>
2017-03-20 22:30:07 -0700

    hw ec1

---
+++ hwEC1/MagicWordEC1.java
@@ -0,0 +1,15 @@
+public class MagicWordEC1 {
+	public static String magicWordMainSurvey() {
+		/* replace magicWord with the magic word given to you when
+		 * you finished the survey. */
+		String magicWordMainSurvey = "";
+		return magicWordMainSurvey;
+	}
+
+	public static String magicWordTASurvey() {
+		/* replace magicWord with the magic word given to you when
+		 * you finished the TA survey. */
+		String magicWordTASurvey = "";
+		return magicWordTASurvey;
+	}	
+} 
