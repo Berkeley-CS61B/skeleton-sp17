0780383b60cf7ebac14208e679853691da1a065e
klmahoney <kmahoney@berkeley.edu>
2017-03-24 16:35:37 -0700

    Update MagicWordEC1.java

--- hwEC1/MagicWordEC1.java
+++ hwEC1/MagicWordEC1.java
@@ -2,14 +2,14 @@ public class MagicWordEC1 {
 	public static String magicWordMainSurvey() {
 		/* replace magicWord with the magic word given to you when
 		 * you finished the survey. */
-		String magicWordMainSurvey = "";
+		String magicWordMainSurvey = "penelope";
 		return magicWordMainSurvey;
 	}
 
 	public static String magicWordTASurvey() {
 		/* replace magicWord with the magic word given to you when
 		 * you finished the TA survey. */
-		String magicWordTASurvey = "";
+		String magicWordTASurvey = "penelope";
 		return magicWordTASurvey;
 	}	
 } 
